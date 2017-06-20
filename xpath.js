dfeb4ce542fe8ccbe01ca9f3c95004c77d2fd861
Yaron Naveh <yaronn01@gmail.com>
2017-06-19 17:40:20 -0700

    remove cc header
    changing the project to MIT license

--- xpath.js
+++ xpath.js
@@ -5,13 +5,6 @@
  *
  * Cameron McCormack <cam (at) mcc.id.au>
  *
- * This work is licensed under the Creative Commons Attribution-ShareAlike
- * License. To view a copy of this license, visit
- * 
- *   http://creativecommons.org/licenses/by-sa/2.0/
- *
- * or send a letter to Creative Commons, 559 Nathan Abbott Way, Stanford,
- * California 94305, USA.
  *
  * Revision 20: April 26, 2011
  *   Fixed a typo resulting in FIRST_ORDERED_NODE_TYPE results being wrong,
